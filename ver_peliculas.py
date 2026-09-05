e7b2223c73fb04b40df71ff80c3ad8c9cd7b7a3b
YamiRam <yamilaramirez848@gmail.com>
2026-09-05 00:41:31 -0300

    Agrego modulo ver peliculas

---
+++ ver_peliculas.py
@@ -0,0 +1,402 @@
+def ver_pelicula():
+
+    print("------ CATALOGO DE PELICULAS ------")
+    print("El Exorcista")
+    print("El Resplandor")
+    print("Pesadilla en Elm Street")
+    print("Scream")
+    print("Saw")
+    print("Un Lugar en Silencio")
+    print("El Conjuro 3")
+    print("Sonríe")
+    print("M3GAN")
+    print("Sinners")
+    print("Mi Pobre Angelito")
+    print("La Máscara")
+    print("Scary Movie")
+    print("Shrek")
+    print("¿Qué pasó ayer?")
+    print("Free Guy")
+    print("Red")
+    print("Barbie")
+    print("Mi Villano Favorito 4")
+    print("Robot Salvaje")
+    print("Seven")
+    print("El Juego")
+    print("El Sexto Sentido")
+    print("El Coleccionista de Huesos")
+    print("Los Otros")
+    print("Fragmentado")
+    print("¡Huye!")
+    print("En la Hierba Alta")
+    print("Corre")
+    print("El Menú")
+    print("Titanic")
+    print("10 Cosas que Odio de Ti")
+    print("Diario de una Pasión")
+    print("Orgullo y Prejuicio")
+    print("Siempre el Mismo Día")
+    print("Yo Antes de Ti")
+    print("A Todos los Chicos de los que me Enamoré")
+    print("A Dos Metros de Ti")
+    print("The Idea of You")
+    print("Duro de Matar")
+    print("Terminator 2: El Juicio Final")
+    print("Misión: Imposible")
+    print("Gladiador")
+    print("Batman: El Caballero de la Noche")
+    print("Mad Max: Furia en el Camino")
+    print("Misión: Imposible - Fallout")
+    print("Top Gun: Maverick")
+    print("John Wick 4")
+    print("Furiosa: De la Saga Mad Max")
+
+
+    pelicula = input("Ingrese el nombre de la película: ")
+    genero = input("Ingrese el género de la película: ")
+
+    if pelicula == "Shrek" and genero == "Comedia":
+        print("Título: Shrek")
+        print("Año: 2001")
+        print("Género: Comedia")
+        print("Duración: 90 minutos")
+        print("Sinopsis: Un ogro emprende una aventura para rescatar a una princesa.")
+
+    elif pelicula == "Mi Pobre Angelito" and genero == "Comedia":
+        print("Título: Mi Pobre Angelito")
+        print("Año: 1990")
+        print("Género: Comedia")
+        print("Duración: 103 minutos")
+        print("Sinopsis: Un niño queda solo en casa y enfrenta a dos ladrones.")
+
+    elif pelicula == "La Máscara" and genero == "Comedia":
+        print("Título: La Máscara")
+        print("Año: 1994")
+        print("Género: Comedia")
+        print("Duración: 101 minutos")
+        print("Sinopsis: Un hombre encuentra una máscara con poderes especiales.")
+
+    elif pelicula == "Scary Movie" and genero == "Comedia":
+        print("Título: Scary Movie")
+        print("Año: 2000")
+        print("Género: Comedia")
+        print("Duración: 88 minutos")
+        print("Sinopsis: Un grupo de jóvenes vive situaciones que parodian películas de terror.")
+
+    elif pelicula == "¿Qué pasó ayer?" and genero == "Comedia":
+        print("Título: ¿Qué pasó ayer?")
+        print("Año: 2009")
+        print("Género: Comedia")
+        print("Duración: 100 minutos")
+        print("Sinopsis: Tres amigos intentan recordar una noche de fiesta.")
+
+    elif pelicula == "Free Guy" and genero == "Comedia":
+        print("Título: Free Guy")
+        print("Año: 2021")
+        print("Género: Comedia")
+        print("Duración: 115 minutos")
+        print("Sinopsis: Un personaje descubre que vive dentro de un videojuego.")
+
+    elif pelicula == "Red" and genero == "Comedia":
+        print("Título: Red")
+        print("Año: 2022")
+        print("Género: Comedia")
+        print("Duración: 100 minutos")
+        print("Sinopsis: Una adolescente se transforma en un panda rojo gigante.")
+
+    elif pelicula == "Barbie" and genero == "Comedia":
+        print("Título: Barbie")
+        print("Año: 2023")
+        print("Género: Comedia")
+        print("Duración: 114 minutos")
+        print("Sinopsis: Barbie abandona su mundo y conoce la vida real.")
+
+    elif pelicula == "Mi Villano Favorito 4" and genero == "Comedia":
+        print("Título: Mi Villano Favorito 4")
+        print("Año: 2024")
+        print("Género: Comedia")
+        print("Duración: 94 minutos")
+        print("Sinopsis: Gru y su familia enfrentan a un nuevo enemigo.")
+
+    elif pelicula == "Robot Salvaje" and genero == "Comedia":
+        print("Título: Robot Salvaje")
+        print("Año: 2024")
+        print("Género: Comedia")
+        print("Duración: 102 minutos")
+        print("Sinopsis: Un robot debe aprender a sobrevivir en la naturaleza.")
+
+    elif pelicula == "El Exorcista" and genero == "Terror":
+        print("Título: El Exorcista")
+        print("Año: 1973")
+        print("Género: Terror")
+        print("Duración: 122 minutos")
+        print("Sinopsis: Una niña comienza a experimentar extraños fenómenos.")
+
+    elif pelicula == "El Resplandor" and genero == "Terror":
+        print("Título: El Resplandor")
+        print("Año: 1980")
+        print("Género: Terror")
+        print("Duración: 146 minutos")
+        print("Sinopsis: Una familia se instala en un hotel aislado.")
+
+    elif pelicula == "Pesadilla en Elm Street" and genero == "Terror":
+        print("Título: Pesadilla en Elm Street")
+        print("Año: 1984")
+        print("Género: Terror")
+        print("Duración: 91 minutos")
+        print("Sinopsis: Un grupo de jóvenes es perseguido durante sus sueños.")
+
+    elif pelicula == "Scream" and genero == "Terror":
+        print("Título: Scream")
+        print("Año: 1996")
+        print("Género: Terror")
+        print("Duración: 111 minutos")
+        print("Sinopsis: Un asesino enmascarado persigue a un grupo de adolescentes.")
+
+    elif pelicula == "Saw" and genero == "Terror":
+        print("Título: Saw")
+        print("Año: 2004")
+        print("Género: Terror")
+        print("Duración: 103 minutos")
+        print("Sinopsis: Dos hombres despiertan atrapados en un extraño juego.")
+
+    elif pelicula == "Un Lugar en Silencio" and genero == "Terror":
+        print("Título: Un Lugar en Silencio")
+        print("Año: 2018")
+        print("Género: Terror")
+        print("Duración: 90 minutos")
+        print("Sinopsis: Una familia debe sobrevivir sin hacer ruido.")
+
+    elif pelicula == "El Conjuro 3" and genero == "Terror":
+        print("Título: El Conjuro 3")
+        print("Año: 2021")
+        print("Género: Terror")
+        print("Duración: 112 minutos")
+        print("Sinopsis: Una pareja investiga un nuevo caso paranormal.")
+
+    elif pelicula == "Sonríe" and genero == "Terror":
+        print("Título: Sonríe")
+        print("Año: 2022")
+        print("Género: Terror")
+        print("Duración: 115 minutos")
+        print("Sinopsis: Una psicóloga comienza a vivir sucesos aterradores.")
+
+    elif pelicula == "M3GAN" and genero == "Terror":
+        print("Título: M3GAN")
+        print("Año: 2022")
+        print("Género: Terror")
+        print("Duración: 102 minutos")
+        print("Sinopsis: Una muñeca inteligente comienza a comportarse de forma extraña.")
+
+    elif pelicula == "Sinners" and genero == "Terror":
+        print("Título: Sinners")
+        print("Año: 2025")
+        print("Género: Terror")
+        print("Duración: 137 minutos")
+        print("Sinopsis: Dos hermanos enfrentan una amenaza sobrenatural.")
+
+    elif pelicula == "Seven" and genero == "Suspenso":
+        print("Título: Seven")
+        print("Año: 1995")
+        print("Género: Suspenso")
+        print("Duración: 127 minutos")
+        print("Sinopsis: Dos detectives investigan una serie de asesinatos.")
+
+    elif pelicula == "El Juego" and genero == "Suspenso":
+        print("Título: El Juego")
+        print("Año: 1997")
+        print("Género: Suspenso")
+        print("Duración: 129 minutos")
+        print("Sinopsis: Un hombre participa en un misterioso juego.")
+
+    elif pelicula == "El Sexto Sentido" and genero == "Suspenso":
+        print("Título: El Sexto Sentido")
+        print("Año: 1999")
+        print("Género: Suspenso")
+        print("Duración: 107 minutos")
+        print("Sinopsis: Un psicólogo ayuda a un niño con un extraño secreto.")
+
+    elif pelicula == "El Coleccionista de Huesos" and genero == "Suspenso":
+        print("Título: El Coleccionista de Huesos")
+        print("Año: 1999")
+        print("Género: Suspenso")
+        print("Duración: 118 minutos")
+        print("Sinopsis: Dos detectives buscan a un peligroso asesino.")
+
+    elif pelicula == "Los Otros" and genero == "Suspenso":
+        print("Título: Los Otros")
+        print("Año: 2001")
+        print("Género: Suspenso")
+        print("Duración: 104 minutos")
+        print("Sinopsis: Una familia vive en una casa donde ocurren hechos extraños.")
+
+    elif pelicula == "Fragmentado" and genero == "Suspenso":
+        print("Título: Fragmentado")
+        print("Año: 2016")
+        print("Género: Suspenso")
+        print("Duración: 117 minutos")
+        print("Sinopsis: Tres jóvenes son secuestradas por un hombre misterioso.")
+
+    elif pelicula == "¡Huye!" and genero == "Suspenso":
+        print("Título: ¡Huye!")
+        print("Año: 2017")
+        print("Género: Suspenso")
+        print("Duración: 104 minutos")
+        print("Sinopsis: Un joven descubre un oscuro secreto familiar.")
+
+    elif pelicula == "En la Hierba Alta" and genero == "Suspenso":
+        print("Título: En la Hierba Alta")
+        print("Año: 2019")
+        print("Género: Suspenso")
+        print("Duración: 101 minutos")
+        print("Sinopsis: Dos hermanos quedan atrapados en un campo misterioso.")
+
+    elif pelicula == "Corre" and genero == "Suspenso":
+        print("Título: Corre")
+        print("Año: 2020")
+        print("Género: Suspenso")
+        print("Duración: 90 minutos")
+        print("Sinopsis: Una adolescente comienza a sospechar de su madre.")
+
+    elif pelicula == "El Menú" and genero == "Suspenso":
+        print("Título: El Menú")
+        print("Año: 2022")
+        print("Género: Suspenso")
+        print("Duración: 107 minutos")
+        print("Sinopsis: Una cena exclusiva toma un rumbo inesperado.")
+
+    elif pelicula == "Titanic" and genero == "Romance":
+        print("Título: Titanic")
+        print("Año: 1997")
+        print("Género: Romance")
+        print("Duración: 194 minutos")
+        print("Sinopsis: Dos jóvenes se enamoran durante un viaje en barco.")
+
+    elif pelicula == "10 Cosas que Odio de Ti" and genero == "Romance":
+        print("Título: 10 Cosas que Odio de Ti")
+        print("Año: 1999")
+        print("Género: Romance")
+        print("Duración: 97 minutos")
+        print("Sinopsis: Una adolescente se enamora mientras intenta cumplir una regla familiar.")
+
+    elif pelicula == "Diario de una Pasión" and genero == "Romance":
+        print("Título: Diario de una Pasión")
+        print("Año: 2004")
+        print("Género: Romance")
+        print("Duración: 123 minutos")
+        print("Sinopsis: Dos jóvenes se enamoran pese a sus diferencias.")
+
+    elif pelicula == "Orgullo y Prejuicio" and genero == "Romance":
+        print("Título: Orgullo y Prejuicio")
+        print("Año: 2005")
+        print("Género: Romance")
+        print("Duración: 129 minutos")
+        print("Sinopsis: Una joven conoce a un hombre que desafía sus prejuicios.")
+
+    elif pelicula == "Siempre el Mismo Día" and genero == "Romance":
+        print("Título: Siempre el Mismo Día")
+        print("Año: 2011")
+        print("Género: Romance")
+        print("Duración: 107 minutos")
+        print("Sinopsis: Dos amigos mantienen un vínculo durante muchos años.")
+
+    elif pelicula == "Yo Antes de Ti" and genero == "Romance":
+        print("Título: Yo Antes de Ti")
+        print("Año: 2016")
+        print("Género: Romance")
+        print("Duración: 110 minutos")
+        print("Sinopsis: Una joven comienza a cuidar a un hombre con discapacidad.")
+
+    elif pelicula == "A Todos los Chicos de los que me Enamoré" and genero == "Romance":
+        print("Título: A Todos los Chicos de los que me Enamoré")
+        print("Año: 2018")
+        print("Género: Romance")
+        print("Duración: 99 minutos")
+        print("Sinopsis: Las cartas secretas de una adolescente son enviadas por accidente.")
+
+    elif pelicula == "A Dos Metros de Ti" and genero == "Romance":
+        print("Título: A Dos Metros de Ti")
+        print("Año: 2019")
+        print("Género: Romance")
+        print("Duración: 116 minutos")
+        print("Sinopsis: Dos jóvenes se enamoran mientras deben mantenerse alejados.")
+
+    elif pelicula == "The Idea of You" and genero == "Romance":
+        print("Título: The Idea of You")
+        print("Año: 2024")
+        print("Género: Romance")
+        print("Duración: 115 minutos")
+        print("Sinopsis: Una mujer inicia una relación inesperada con un cantante.")
+
+    elif pelicula == "Duro de Matar" and genero == "Acción":
+        print("Título: Duro de Matar")
+        print("Año: 1988")
+        print("Género: Acción")
+        print("Duración: 132 minutos")
+        print("Sinopsis: Un policía enfrenta a un grupo de criminales.")
+
+    elif pelicula == "Terminator 2: El Juicio Final" and genero == "Acción":
+        print("Título: Terminator 2: El Juicio Final")
+        print("Año: 1991")
+        print("Género: Acción")
+        print("Duración: 137 minutos")
+        print("Sinopsis: Un joven es protegido de una amenaza del futuro.")
+
+    elif pelicula == "Misión: Imposible" and genero == "Acción":
+        print("Título: Misión: Imposible")
+        print("Año: 1996")
+        print("Género: Acción")
+        print("Duración: 110 minutos")
+        print("Sinopsis: Un agente intenta descubrir quién traicionó a su equipo.")
+
+    elif pelicula == "Gladiador" and genero == "Acción":
+        print("Título: Gladiador")
+        print("Año: 2000")
+        print("Género: Acción")
+        print("Duración: 155 minutos")
+        print("Sinopsis: Un general romano busca vengarse de quienes lo traicionaron.")
+
+    elif pelicula == "Batman: El Caballero de la Noche" and genero == "Acción":
+        print("Título: Batman: El Caballero de la Noche")
+        print("Año: 2008")
+        print("Género: Acción")
+        print("Duración: 152 minutos")
+        print("Sinopsis: Batman enfrenta a un peligroso criminal.")
+
+    elif pelicula == "Mad Max: Furia en el Camino" and genero == "Acción":
+        print("Título: Mad Max: Furia en el Camino")
+        print("Año: 2015")
+        print("Género: Acción")
+        print("Duración: 120 minutos")
+        print("Sinopsis: Un grupo intenta escapar por un mundo postapocalíptico.")
+
+    elif pelicula == "Misión: Imposible - Fallout" and genero == "Acción":
+        print("Título: Misión: Imposible - Fallout")
+        print("Año: 2018")
+        print("Género: Acción")
+        print("Duración: 147 minutos")
+        print("Sinopsis: Un agente debe detener una amenaza mundial.")
+
+    elif pelicula == "Top Gun: Maverick" and genero == "Acción":
+        print("Título: Top Gun: Maverick")
+        print("Año: 2022")
+        print("Género: Acción")
+        print("Duración: 131 minutos")
+        print("Sinopsis: Un piloto entrena a una nueva generación.")
+
+    elif pelicula == "John Wick 4" and genero == "Acción":
+        print("Título: John Wick 4")
+        print("Año: 2023")
+        print("Género: Acción")
+        print("Duración: 169 minutos")
+        print("Sinopsis: John Wick lucha por recuperar su libertad.")
+
+    elif pelicula == "Furiosa: De la Saga Mad Max" and genero == "Acción":
+        print("Título: Furiosa: De la Saga Mad Max")
+        print("Año: 2024")
+        print("Género: Acción")
+        print("Duración: 148 minutos")
+        print("Sinopsis: Una joven lucha por sobrevivir en un mundo hostil.")
+
+    else:
+        print("La película no se encuentra en el catálogo.")
