decae2f81d2128220e16cc58d07ff7199f8dce5c
cayovalentina08-cpu <cayovalentina08@gmail.com>
2026-09-05 00:06:03 -0300

    Update sistema_pizzeria.py2

--- sistema_pizzeria.py
+++ sistema_pizzeria.py
@@ -5,3 +5,20 @@ def mostrar_menu():
     print("3. Pizza grande  - $10000")
 
 mostrar_menu()
+
+
+
+def calcular_descuento(subtotal):
+    if subtotal >= 30000:
+        descuento = subtotal * 0.10
+    else:
+        descuento = 0
+
+    return descuento
+
+
+cantidad = int(input("Cantidad de pizzas: "))
+
+while cantidad <= 0:
+    print("La cantidad debe ser mayor que 0.")
+    cantidad = int(input("Cantidad de pizzas: "))
